3e6128c69f19f2f9189dd9be2677e6f84319cf89
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-02-02 23:32:59 +0100

    Började med JS

---
+++ script.js
@@ -0,0 +1,44 @@
+const poängBtn = document.querySelector('.se-poäng-knapp')
+const andraFönstret = document.querySelector('.visa-efter-start')
+const inputNamn = document.querySelector('#spelarens-namn')
+const spelaBtn = document.querySelector('.börja-spelet-knapp')
+const förstaFönstret = document.querySelector('.upp-efter-klick')
+const parts = {
+  partEtt: document.querySelector('.part1'),
+  partTvå: document.querySelector('.part2'),
+  partTre: document.querySelector('.part3'),
+  partFyra: document.querySelector('.part4'),
+  partFem: document.querySelector('.part5'),
+}
+
+
+// Här kan man sätta dem till none så att dem ej visas
+// och sedan så kan man använda if för att få fram dem.
+parts.partEtt.style.display = 'block'
+
+
+förstaFönstret.style.display = ''
+andraFönstret.style.display = 'none'
+
+
+spelaBtn.addEventListener('click', () =>{
+  const spelarensNamn = inputNamn.value
+})
+
+spelaBtn.addEventListener('click', function(){
+  localStorage.setItem('inputValue', inputNamn.value)
+})
+
+
+
+function toggleSections() {
+  if (förstaFönstret.style.display === 'none') {
+    andraFönstret.style.display = ''
+  } else {
+    förstaFönstret.style.display = 'none'
+    andraFönstret.style.display = 'block'
+  }
+}
+spelaBtn.addEventListener('click', toggleSections)
+
+
