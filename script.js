5baf48b6407fcd240abd2e09cc922f7a58e88fc7
Linn Gadevik <gadeviklinn@gmail.com>
2023-02-07 10:10:10 +0100

    klar med overlay! -text

--- script.js
+++ script.js
@@ -136,3 +136,17 @@ button.addEventListener('click', event => {
   console.log('Du klickade på knappen')
   console.log(event)
 } )
+
+document,addEventListener('DOMContentLoaded',() => {
+const openBtn = document.querySelector('#openOverlay');
+const closeBtn = document.querySelector('#closeOverlay');
+const overlay = document.querySelector('.overlay');
+
+openBtn.addEventListener('click', () => {
+overlay.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+  overlay.style.display = 'none';
+})
+});
